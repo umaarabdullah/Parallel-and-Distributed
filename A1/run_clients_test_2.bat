55c5ec36a783225006b944c49b146bfc6c97b89c
umaarabdullah <umaar71@gmail.com>
2023-07-31 18:26:11 +0600

    init

--- A1/run_clients_test_2.bat
+++ A1/run_clients_test_2.bat
@@ -1,7 +1,7 @@
 @echo off
 
 start "client1_test_2" cmd /k cd /D "Client_Server_Parallel/tests" ^& make run_client1_test_2
-start "client2_test_2" cmd /k cd /D "Client_Server_Parallel/tests" ^& make run_client2_test_2
-start "client3_test_2" cmd /k cd /D "Client_Server_Parallel/tests" ^& make run_client3_test_2
+@REM start "client2_test_2" cmd /k cd /D "Client_Server_Parallel/tests" ^& make run_client2_test_2
+@REM start "client3_test_2" cmd /k cd /D "Client_Server_Parallel/tests" ^& make run_client3_test_2
 
 echo "Test 2 Completed"
