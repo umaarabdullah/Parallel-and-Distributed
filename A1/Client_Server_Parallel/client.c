c53a525ff0e692a65277368f60106a3875b8b604
Umaar_Abdullah <umaar71@gmail.com>
2023-08-02 18:43:21 +0600

    cross-platform-support-linux-und-windows

--- A1/Client_Server_Parallel/client.c
+++ A1/Client_Server_Parallel/client.c
@@ -2,26 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <winsock2.h>
 
-// #define SERVER_IP "127.0.0.1"
+#ifdef _WIN32
+#include <winsock2.h>
+#else
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#endif
+
 #define SERVER_IP "192.168.68.107"
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
 int main() {
+#ifdef _WIN32
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         perror("WSAStartup failed");
         return 1;
     }
+#endif
 
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
     char buffer[BUFFER_SIZE] = {0};
 
     // Create a socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("socket failed");
         return 1;
     }
@@ -36,14 +45,14 @@ int main() {
     }
 
     // Connect to the server
-    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR) {
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
         perror("connection failed");
         return 1;
     }
 
     printf("Connected to the server\n");
-    
-    char ackMsg[] = "ack";      // ack and nack will keep the client informed whether client gave the correct response to server or not
+
+    char ackMsg[] = "ack";  // ack and nack will keep the client informed whether client gave the correct response to server or not
     bool ackFlag = true;
     int sequence_num = 0;
     char clientTerminationMsg[] = "I have no more jokes to tell.\n";
@@ -53,75 +62,74 @@ int main() {
         // Receive a response from the server
         valread = recv(sock, buffer, BUFFER_SIZE, 0);
         if (valread <= 0) {
-            break; // Server closed the connection or an error occurred
+            break;  // Server closed the connection or an error occurred
         }
         printf("Server: %s", buffer);
-        
-        if(strcmp(buffer, clientTerminationMsg) == 0)
+
+        if (strcmp(buffer, clientTerminationMsg) == 0)
             break;  // shutdown client
 
-        memset(buffer, 0, BUFFER_SIZE); // Clear the buffer
+        memset(buffer, 0, BUFFER_SIZE);  // Clear the buffer
 
-        if(ackFlag){    // send response to server
-            if(sequence_num == 2){      // sequence_num implies (Y/N) question so check response validity on the client side
-                while(1){
+        if (ackFlag) {  // send response to server
+            if (sequence_num == 2) {  // sequence_num implies (Y/N) question so check response validity on the client side
+                while (1) {
                     printf("You: ");
                     fgets(buffer, BUFFER_SIZE, stdin);  // fgets takes newline as input
-                    if(buffer[0] == 'n' || buffer[0] == 'N' || 
-                        buffer[0] == 'y' || buffer[0] == 'Y'){
-                            if(buffer[1] == '\n')
-                                break;
-                            else
-                                printf("Server: Invalid Response! Only (Y/N) response applicable.\n");
-                    }
-                    else{
+                    if (buffer[0] == 'n' || buffer[0] == 'N' || buffer[0] == 'y' || buffer[0] == 'Y') {
+                        if (buffer[1] == '\n')
+                            break;
+                        else
+                            printf("Server: Invalid Response! Only (Y/N) response applicable.\n");
+                    } else {
                         printf("Server: Invalid Response! Only (Y/N) response applicable.\n");
                     }
                 }
-            }
-            else{
+            } else {
                 printf("You: ");
-                fgets(buffer, BUFFER_SIZE, stdin);  // fgets takes newline as input 
+                fgets(buffer, BUFFER_SIZE, stdin);  // fgets takes newline as input
             }
 
             // Send the message to the server
             send(sock, buffer, strlen(buffer), 0);
-        }
-        else{           // Receive a response from the server
+        } else {  // Receive a response from the server
             valread = recv(sock, buffer, BUFFER_SIZE, 0);
             if (valread <= 0) {
-                break; // Server closed the connection or an error occurred
+                break;  // Server closed the connection or an error occurred
             }
             printf("Server: %s", buffer);
-            memset(buffer, 0, BUFFER_SIZE); // Clear the buffer
+            memset(buffer, 0, BUFFER_SIZE);  // Clear the buffer
 
             printf("You: ");
-            fgets(buffer, BUFFER_SIZE, stdin);  // fgets takes newline as input 
+            fgets(buffer, BUFFER_SIZE, stdin);  // fgets takes newline as input
             // Send the message to the server
             send(sock, buffer, strlen(buffer), 0);
-            memset(buffer, 0, BUFFER_SIZE); // Clear the buffer
+            memset(buffer, 0, BUFFER_SIZE);  // Clear the buffer
         }
 
-        memset(buffer, 0, BUFFER_SIZE); // Clear the buffer
+        memset(buffer, 0, BUFFER_SIZE);  // Clear the buffer
         // check for acknowledgment
         valread = recv(sock, buffer, BUFFER_SIZE, 0);
         if (valread <= 0) {
-            break; // Server closed the connection or an error occurred
+            break;  // Server closed the connection or an error occurred
         }
-        if(strcmp(buffer, ackMsg) == 0){    // ack received
+        if (strcmp(buffer, ackMsg) == 0) {  // ack received
             ackFlag = true;
             sequence_num++;
-            if(sequence_num == 3)   // each jokes can go upto highest sequence number 3 then a new joke will start
+            if (sequence_num == 3)  // each jokes can go upto highest sequence number 3 then a new joke will start
                 sequence_num = 0;
-        }
-        else{                               // nack received
+        } else {  // nack received
             ackFlag = false;
             sequence_num = 0;
         }
     }
 
+#ifdef _WIN32
     closesocket(sock);
     WSACleanup();
+#else
+    close(sock);
+#endif
 
     return 0;
 }
