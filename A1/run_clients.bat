b58d11af2d20fa95e641ade63385626dcb74b57f
umaarabdullah <umaar71@gmail.com>
2023-10-03 21:56:45 +0600

    init

--- A1/run_clients.bat
+++ A1/run_clients.bat
@@ -3,9 +3,6 @@
 rem Execute the run_server target in a new terminal window
 @REM start cmd /k cd /D "Client_Server_Parallel" ^& make run_server
 
-rem Add a delay to ensure the server has started before running clients (adjust as needed)
-timeout /t 5
-
 rem Execute the run_client1 target in a new terminal window
 start cmd /k cd /D "Client_Server_Parallel" ^& make run_client1
 
--- A1/run_clients_test_1.bat
+++ A1/run_clients_test_1.bat
@@ -2,10 +2,6 @@
 
 rem Test if all 30 jokes are sent and received
 
-@REM start "server" cmd /k cd /D "Client_Server_Parallel" ^& make run_server
-
-timeout /t 5
-
 start "client1_test_1" cmd /k cd /D "Client_Server_Parallel/tests" ^& make run_client1_test_1
 start "client2_test_1" cmd /k cd /D "Client_Server_Parallel/tests" ^& make run_client2_test_1
 start "client3_test_1" cmd /k cd /D "Client_Server_Parallel/tests" ^& make run_client3_test_1
